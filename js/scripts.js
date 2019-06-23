de5a8cfde804e0d5b229168080cd37f901f4392e
Rocky Fatafehi <coder97006@gmail.com>
2019-06-22 18:12:08 -0700

    Mostly completed basic behavior for each list item, needs more styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,38 @@
 $( document ).ready(function() {
     
-    $(".clicks").click(function() {
-        $("#javascript").hide();
-        $("#javascriptDef").show();
+    $(".clicks").click(function() {       
+        $("#javascript").toggle();
+        $("#javascriptDef").toggle();
       });
-    
+
+    $(".clicks2").click(function() {       
+        $("#oper").toggle();
+        $("#operDef").toggle();
+      });
   
+      $(".clicks3").click(function() {       
+        $("#vari").toggle();
+        $("#variDef").toggle();
+      });
 
+      $(".clicks4").click(function() {       
+        $("#function").toggle();
+        $("#functionDef").toggle();
+      });
 
+      $(".clicks5").click(function() {       
+        $("#method").toggle();
+        $("#methodDef").toggle();
+      });
 
+      $(".clicks6").click(function() {       
+        $("#jquery").toggle();
+        $("#jqueryDef").toggle();
+      });
+
+      $(".clicks7").click(function() {       
+        $("#boolean").toggle();
+        $("#booleanDef").toggle();
+      });
 
 });
